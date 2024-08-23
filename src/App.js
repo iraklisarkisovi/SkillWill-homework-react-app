172c35ea4438468a282cd07d81af05ecec7e6ea4
iraklisarkisovi <sarqisovirakli@gmail.com>
2024-08-24 00:03:16 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,35 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='personalCard'>
+          <img src='logolime.png'></img>
+            <h1>Hi! im Irakli</h1>
+            <h3>These are my <b>SMART goals</b></h3>
+            <p>
+            <strong>Specific: </strong>
+            My main goal is to become a programmer and I started my way in SkillWill, studying Front-end development. I`m interested in programming since childhood so I decided that I must reach my main goal.
+            </p>
+
+            <p>
+            <strong>Measurable: </strong>
+            Since I introduced myself I would like to tell how often I study programming. It takes around 2-3 hours per day sometimes 3-4 hours of work.  
+            </p>
+
+            <p>
+            <strong>Attainable: </strong>
+            I believe my goal is achievable because I don't give up, and I'm pursuing it with all my heart. There are times when I make mistakes and feel like giving up, but then I realize that mistakes are opportunities for learning and getting motivated to keep going and not giving up that easily.
+            </p>
+
+            <p>
+            <strong>Relevant: </strong>
+            I'm dedicated to becoming a programmer because I love this profession. Besides this, I don't see anything else I'd rather be than a programmer.
+            </p>
+
+            <p>
+            <strong>Time bound: </strong>
+            I started studying Python when I was 14, and it`s helping me now with JavaScript. I'm 17 now and planning to become a professional programmer in 1-2 years, hoping to start working with SkillWill.
+            </p>
+        </div>
       </header>
     </div>
   );
